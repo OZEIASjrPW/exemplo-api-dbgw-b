64ae81a71e676bf9c33057bf5b7eaa04ca567261
OZEIASjrPW <ozeias.santos5@etec.sp.gov.br>
2021-11-12 11:34:19 +0000

    Criação do Banco de Dados

--- assets/sql/init_data.sql
+++ assets/sql/init_data.sql
@@ -1,1 +1,7 @@
 -- Código SQL com a inclusão dos registros iniciais do banco de dados
+
+INSERT INTO filmes(titulo) VALUES
+    ('Entre facas e Segredos'),
+    ('O Hobbit'),
+    ('Guia do Mochileiro das Galáxias'),
+    ('Monty Python: Em busca do Cálice Sagrado');
